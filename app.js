acf5a773a76c05ff8f5340a5036bf41f4142cec3
Divya3944 <ddchinviya@gmail.com>
2021-09-03 20:19:10 +0530

    logical operators

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-a=4
-b=7
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
+const a=2
+const b=2
+console.log('a==b',a==b)
+console.log('a!=b',a!=b)
 
-console.log(a%b)
-console.log(a**b)
-console.log(a===b)
+
+console.log('false && false',false&&false)
+console.log('false&&true',false&&true)
+console.log('true&&true',true&&true)
+console.log('true&&true',true&&true)
