453f67a45f8d314a5575196c03c4ab0725c69a6d
Divya3944 <ddchinviya@gmail.com>
2021-09-03 19:28:54 +0530

    javascript operators

--- app.js
+++ app.js
@@ -1,18 +1,2 @@
-//primitive data type
-var name="ice cream parlour"
-
-console.log(name)
-let age=21
-console.log(age)
-console.log(typeof age)
-let mam='e'
-console.log(mam)
-let car="unovo"
-console.log(car)
-
-//non primitive data type
-let arr=[1,2,3,4,]
-arr.push("value")
-arr.push(false)
-console.log(arr)
-console.log(typeof arr)
+let name="dappa"
+console.log(name.toUppercase())
