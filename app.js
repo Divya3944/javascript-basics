36fcc6e47b8425ccd9e0925842b0a5bd014bddf3
Divya3944 <ddchinviya@gmail.com>
2021-09-27 12:12:00 +0530

    node methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// const element = document.getElementById('click')
-// console.log(element)
-// const header = document.getElementsByClassName('heading')
-// console.log(header)
-const element = document.querySelector('.heading')
-console.log(element)
+
+// element.innerHTML="hi my dear"
+// console.style = "display:none"
+// console.style.color = "cyan"
+// console.style.fontSize = "40px"
+// const element = document.querySelector('#selectme')
+// console.log(element.classList)
+// element.classList.add('six')
+// console.log(element.classList)
+const body = document.querySelector('body')
+console.log(body)
+console.log(body.classList.toggle('dark'))
