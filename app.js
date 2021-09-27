24ab23b74a2e9714b45bbecf6afd4984fc07b277
Divya3944 <ddchinviya@gmail.com>
2021-09-27 21:42:46 +0530

    dark mode

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
+const button = document.querySelector('button')
+const element = document.querySelector('body')
 
-// element.innerHTML="hi my dear"
-// console.style = "display:none"
-// console.style.color = "cyan"
-// console.style.fontSize = "40px"
-// const element = document.querySelector('#selectme')
-// console.log(element.classList)
-// element.classList.add('six')
-// console.log(element.classList)
-const body = document.querySelector('body')
-console.log(body)
-console.log(body.classList.toggle('dark'))
+
+button.addEventListener('click',()=>{
+    element.classList.toggle('dark')
+   
+})
